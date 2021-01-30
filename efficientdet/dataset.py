e07b9df6ac862f58f97bffea6650ab3357779196
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2021-01-30 21:58:15 +1300

    Update dataset.py

--- efficientdet/dataset.py
+++ efficientdet/dataset.py
@@ -49,11 +49,12 @@ class CocoDataset(Dataset):
     def load_image(self, image_index):
         image_info = self.coco.loadImgs([self.image_ids[image_index]])[0]
         path = os.path.join(self.root_dir, self.set_name, image_info['file_name'])
-        img = cv2.imread(path, -1)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
-        print('file_name', image_info['file_name'])
-        print('path', path)
+        try:
+            img = cv2.imread(path, -1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        except:
+            print('file_name', image_info['file_name'])
+            print('path', path)
 
         return img.astype(np.float32) / 255.
 
